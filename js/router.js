fb4a630778fd6a293468842489cb6e71789ebc91
unknown <rajeshnalleboina94@gmail.com>
2022-12-01 22:27:36 +0530

    demo3

--- js/router.js
+++ js/router.js
@@ -7,14 +7,14 @@ const route = (event) => {
 };
 
 const routes = {
-  404: "/pages/404.html",
-  "/": "/pages/index.html",     
-  "/contact": "/pages/contact.html",
-  "/about": "/pages/about.html",
-  "/get-involved": "/pages/getInvolved.html",
-  "/saaf-hyderabad": "/pages/projects/saafHyderabad.html",
-  "/child-protection": "/pages/child-protection.html",
-  "/gallery": "/pages/gallery.html",
+  404: "../pages/404.html",
+  "/": "../pages/index.html",     
+  "/contact": "../pages/contact.html",
+  "/about": "../pages/about.html",
+  "/get-involved": "../pages/getInvolved.html",
+  "/saaf-hyderabad": "../pages/projects/saafHyderabad.html",
+  "/child-protection": "../pages/child-protection.html",
+  "/gallery": "../pages/gallery.html",
 };
 
 const handleLocation = async () => {
